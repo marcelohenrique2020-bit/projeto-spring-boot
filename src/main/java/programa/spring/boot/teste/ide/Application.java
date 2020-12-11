00d66555b504b309edbc89fe341e7ea7f45430e8
marcelohenrique2020 bits <mhpb@aluno.ueg.br>
2020-12-11 15:53:22 -0300

    teste

---
+++ src/main/java/programa/spring/boot/teste/ide/Application.java
@@ -0,0 +1,13 @@
+package programa.spring.boot.teste.ide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/programa/spring/boot/teste/ide/ApplicationTests.java
@@ -0,0 +1,13 @@
+package programa.spring.boot.teste.ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
